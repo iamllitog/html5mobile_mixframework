98b0121e7e6248ca7f21033e441d0dbd2ec6bb60
litong <litong.forme@gmail.com>
2014-10-16 23:08:58 +0800

    表单-内联表单

---
+++ www/js/form.js
@@ -0,0 +1,10 @@
+var page = {
+    initialize: function() {
+        this.bindEvents();
+    },
+    bindEvents: function() {
+        $('#formBt').click(function(){
+            //location.href = 'form.html';
+        });
+    }
+};
