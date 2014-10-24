0007ccb0a05efad1fa9457ff3ca5c11daf0720a1
litong <litong.forme@gmail.com>
2014-10-24 18:08:32 +0800

    finish tabgroup

--- www/framework/mixframework/js/README.md
+++ www/framework/mixframework/js/README.md
@@ -59,14 +59,95 @@
 ###事件
 
 + slideview-movestart
+
     > 开始触摸
 + slideview-movein-leftview
+
     > 划动进入左界面
 + slideview-movein-rightview
+
     > 划动进入右界面
 + slideview-movein-centerview
+
     > 划动进入中间界面
 + slideview-move
+
     > 划动时
 + slideview-moveend
+
     > 划动结束
+
+##TabGroup
+
+###使用方法
+
+1.引入
+
+> `<script src="../framework/mixframework/js/tabgroup.js"></script>`
+
+2.在html标签中嵌入
+
+
+    <div id='tabgroupwrapper'>
+        <div data-tabgroup-tabviews>
+            <div data-tabgroup-tabid='a'></div>
+            <div data-tabgroup-tabid='b'></div>
+            <div data-tabgroup-tabid='c'></div>
+            <div data-tabgroup-tabid='d'></div>
+        </div>
+
+        <div data-tabgroup-tabbuttons>
+            <div data-tabgroup-tabid="a" data-tabgroup-activebtclass="activeBtA" data-tabgroup-defaultbtclass="defaultBtA">
+            </div>
+            <div data-tabgroup-tabid="b" data-tabgroup-activebtclass="activeBtB" data-tabgroup-defaultbtclass="defaultBtB">
+            </div>
+            <div data-tabgroup-tabid="c" data-tabgroup-activebtclass="activeBtC" data-tabgroup-defaultbtclass="defaultBtC">
+            </div>
+            <div data-tabgroup-tabid="d" data-tabgroup-activebtclass="activeBtD" data-tabgroup-defaultbtclass="defaultBtD">
+            </div>
+        </div>
+    </div>
+
+3.在body结束标签前初始化
+
+    > `var TabGroup = new TabGroup('#tabgroupwrapper');`
+
+    ###初始化参数
+
+    + snapThreshold
+        > 在划动多少距离后，触发menu弹出或隐藏，接受百分比(20%)和像素(50)
+
+    + currentViewId
+        > 设置当前显示界面的id
+
+    + currentViewIndex
+        > 设置当前显示view的index,currentViewId和currentViewIndex都设置时,以currentViewId为准
+
+    + moveMode
+        > 设置切换page的模式
+        >> TabGroup.MOVE_MODE_SLIDE 划动切换
+        >> TabGroup.MOVE_MODE_NODE 禁止划动切换
+
+    ###方法
+
+    + goToPageById(pageId)
+        > 根据Id跳转到指定view
+
+    + goToPageByIndex(index)
+        > 根据index跳转到指定view
+
+    + animationToPageById(pageId)
+        > 根据Id跳转到指定view(动画方式)
+
+    + animationToPageByIndex(index)
+        > 根据index跳转到指定view(动画方式)
+
+    + getCurrentViewId()
+        > 得到当前显示的view的id
+
+    + getCurrentViewIndex()
+        > 得到当前显示的view的index
+
+    ###属性
+
+    ###事件
--- www/framework/mixframework/js/tabgroup.js
+++ www/framework/mixframework/js/tabgroup.js
@@ -36,6 +36,7 @@ var TabGroup = (function (window, document) {
         moveEvent = hasTouch ? 'touchmove' : 'mousemove',
         endEvent = hasTouch ? 'touchend' : 'mouseup',
         cancelEvent = hasTouch ? 'touchcancel' : 'mouseup',
+        tabBtClickEvent = 'click',
         transitionEndEvent = (function () {
             if (vendor === false) return false;
 
@@ -62,7 +63,10 @@ var TabGroup = (function (window, document) {
             var views = document.querySelectorAll(element + " > [data-tabgroup-tabviews] > [data-tabgroup-tabid]");
 
             this.options = {
-                snapThreshold: null //滑动多少距离后出现menu
+                snapThreshold: null, //滑动多少距离后出现menu
+                currentViewId: null,
+                currentViewIndex: 0,
+                moveMode: TabGroup.MOVE_MODE_SLIDE
             };
 
             for (var i in options) this.options[i] = options[i];
@@ -71,42 +75,104 @@ var TabGroup = (function (window, document) {
             this.wrapper.style.position = 'relative';
 
             this.sliderView.style.cssText += ';position:relative;top:0;height:100%;width:100%;' + cssVendor + 'transition-duration:0;' + cssVendor + 'transform:translateZ(0);' + cssVendor + 'transition-timing-function:ease-out';
-            for(var i= 0,j=views.length;i<j;i++){
+            for (var i = 0, j = views.length; i < j; i++) {
                 var view = views[i];
                 var tabId = view.getAttribute('data-tabgroup-tabid');
-                var tabBt = document.querySelector(element + " > [data-tabgroup-tabbuttons] > [data-tabgroup-tabid="+tabId+"]");
-                view.style.cssText += ';'+cssVendor + 'transform:translateZ(0);position:absolute;top:0;height:100%;width:100%;';
-                view.style.left = i*100 + '%';
+                var tabBt = document.querySelector(element + " > [data-tabgroup-tabbuttons] > [data-tabgroup-tabid=" + tabId + "]");
+                view.style.cssText += ';' + cssVendor + 'transform:translateZ(0);position:absolute;top:0;height:100%;width:100%;';
+                view.style.left = i * 100 + '%';
+
                 this.views[tabId] = {
-                    index : i,
-                    tabBt : tabBt,
-                    view : view
+                    index: i,
+                    tabBt: tabBt,
+                    view: view
                 };
+                this.tabIds.push(tabId);
+
+                if (tabBt) {
+                    tabBt.addEventListener(tabBtClickEvent, this, false);
+                    this.views[tabId].tabBtActiveClass = tabBt.getAttribute('data-tabgroup-activebtclass');
+                    this.views[tabId].tabBtDefaultClass = tabBt.getAttribute('data-tabgroup-defaultbtclass');
+                }
+                if (i == 0) {
+                    this.currentViewIndex = 0;
+                    tabBt.className += " " + this.views[tabId].tabBtActiveClass;
+                } else {
+                    tabBt.className += " " + this.views[tabId].tabBtDefaultClass;
+                }
+
             }
+
+            this.viewLength = views.length;
             views = null;
 
             this.refreshSize();
 
             window.addEventListener(resizeEvent, this, false);
-            this.wrapper.addEventListener(startEvent, this, false);
-            this.wrapper.addEventListener(moveEvent, this, false);
-            this.wrapper.addEventListener(endEvent, this, false);
+            this.sliderView.addEventListener(startEvent, this, false);
+            this.sliderView.addEventListener(moveEvent, this, false);
+            this.sliderView.addEventListener(endEvent, this, false);
+
+            if (this.options.currentViewId) {
+                this.goToPageById(this.options.currentViewId);
+            } else if (this.options.currentViewIndex) {
+                this.goToPageByIndex(this.options.currentViewIndex);
+            }
         };
 
+    TabGroup.MOVE_MODE_SLIDE = 1000;
+    TabGroup.MOVE_MODE_NODE = -1000;
+    TabGroup.CHANGE_PAGE_TIME = 300;
+
     TabGroup.prototype = {
-        views : {},
+        views: {},
+        tabIds: [],
+        currentViewIndex: 0,
+        x: 0,
         refreshSize: function () {
-            var wrapperWidth = this.wrapper.clientWidth;
+            this.wrapperWidth = this.wrapper.clientWidth;
             this.snapThreshold = this.options.snapThreshold === null ?
-                Math.round(wrapperWidth * 0.15) :
+                Math.round(this.wrapperWidth * 0.15) :
                 /%/.test(this.options.snapThreshold) ?
-                    Math.round(wrapperWidth * this.options.snapThreshold.replace('%', '') / 100) :
+                    Math.round(this.wrapperWidth * this.options.snapThreshold.replace('%', '') / 100) :
                     this.options.snapThreshold;
-//             for(var i in this.views){
-//                 this.views[i].view.style.width = wrapperWidth;
-// this.views[i].view.style.left = this.views[i].index * wrapperWidth;
-//             }
         },
+
+        //根据pageid跳转到对应界面
+        goToPageById: function (pageId) {
+            var view = this.views[pageId];
+            if (view)
+                this.__changeTab(view.index);
+        },
+        //根据index跳转到对应界面
+        goToPageByIndex: function (pageIndex) {
+            this.__changeTab(pageIndex);
+        },
+
+        //根据pageid跳转到对应界面(动画)
+        animationToPageById: function (pageId) {
+            var view = this.views[pageId];
+            if (view) {
+                this.sliderView.style[transitionDuration] = TabGroup.CHANGE_PAGE_TIME + 'ms';
+                this.__changeTab(view.index);
+            }
+        },
+        //根据index跳转到对应界面(动画)
+        animationToPageByIndex: function (pageIndex) {
+            this.sliderView.style[transitionDuration] = TabGroup.CHANGE_PAGE_TIME + 'ms';
+            this.__changeTab(pageIndex);
+        },
+
+        //得到当前view的id
+        getCurrentViewId: function () {
+            return this.tabIds[this.currentViewIndex];
+        },
+
+        //得到当前view的index
+        getCurrentViewIndex: function () {
+            return this.currentViewIndex;
+        },
+
         //处理事件的方法
         handleEvent: function (e) {
             switch (e.type) {
@@ -123,40 +189,133 @@ var TabGroup = (function (window, document) {
                 case resizeEvent:
                     this.__resize();
                     break;
+                case tabBtClickEvent:
+                    this.__tabBtclick(e);
+                    break;
             }
+        },
+
+        __tabBtclick: function (e) {
+            var tabIndex = this.views[e.currentTarget.getAttribute('data-tabgroup-tabid')].index;
+
+            if (this.options.moveMode == TabGroup.MOVE_MODE_NODE) {
+                this.sliderView.style[transitionDuration] = '0ms';
+            } else {
+                this.sliderView.style[transitionDuration] = TabGroup.CHANGE_PAGE_TIME + 'ms';
+            }
+            this.goToPageByIndex(tabIndex);
         },
 
         __resize: function () {
             this.refreshSize();
             this.sliderView.style[transitionDuration] = '0s';
-            this.__pos(0);
+            this.goToPageByIndex(this.currentViewIndex);
         },
 
         __start: function (e) {
             if (this.initiated) return;
 
+            if (this.options.moveMode == TabGroup.MOVE_MODE_NODE) {
+                return;
+            }
             this.initiated = true;
-
+            var point = hasTouch ? e.touches[0] : e;
+            this.startX = point.pageX;
+            this.startY = point.pageY;
+            this.beginX = point.pageX;
+            this.beginY = point.pageY;
+            this.touchdistanceX = 0;
+            this.touchdistanceY = 0;
+            this.sliderView.style[transitionDuration] = '0s';
         },
 
         __move: function (e) {
 
             if (!this.initiated) return;
 
+            if (this.options.moveMode == TabGroup.MOVE_MODE_NODE) {
+                return;
+            }
+
+            var point = hasTouch ? e.touches[0] : e;
+
+            this.endX = point.pageX;
+            this.endY = point.pageY;
+            this.movedistanceX = this.endX - this.startX;
+            this.movedistanceY = this.endY - this.startY;
+
+            this.newX = this.movedistanceX + this.x;
+
+            this.startX = this.endX;
+            this.startY = this.endY;
+
+            this.touchdistanceX += Math.abs(this.movedistanceX);
+            this.touchdistanceY += Math.abs(this.movedistanceY);
+
+            //x方向和y方向划动大于10为有效
+            if (this.touchdistanceX < 10 && this.touchdistanceY < 10) {
+                return;
+            }
+            e.preventDefault();
+
+            // 越界判断
+            if (this.newX < (-(this.viewLength - 1) * this.wrapperWidth) || this.newX > 0) {
+                return;
+            }
+
+            this.__pos(this.newX);
+
 
         },
 
         __end: function (e) {
             if (!this.initiated) return;
+
+            if (this.options.moveMode == TabGroup.MOVE_MODE_NODE) {
+                return;
+            }
+
             this.initiated = false;
 
+            var point = hasTouch ? e.changedTouches[0] : e,
+                distX = point.pageX - this.beginX;
 
+            this.sliderView.style[transitionDuration] = Math.floor(100 * Math.abs(distX) / this.snapThreshold) + 'ms';
+
+            if (Math.abs(distX) <= this.snapThreshold) {
+                this.__changeTab(this.currentViewIndex);
+            } else if (distX > 0) {
+                this.__changeTab(this.currentViewIndex > 0 ? (this.currentViewIndex - 1) : 0);
+            } else {
+                this.__changeTab(this.currentViewIndex < (this.viewLength - 1) ? (this.currentViewIndex + 1) : (this.viewLength - 1));
+            }
+        },
+
+        //更改tab
+        __changeTab: function (newIndex) {
+            if (this.currentViewIndex != newIndex) {
+
+                var currentView = this.views[this.tabIds[this.currentViewIndex]];
+                var newView = this.views[this.tabIds[newIndex]];
+
+                var activeClass = currentView.tabBtActiveClass;
+                var defualtClass = currentView.tabBtDefaultClass;
+                currentView.tabBt.className = currentView.tabBt.className.replace(activeClass, defualtClass);
+
+                activeClass = newView.tabBtActiveClass;
+                defualtClass = newView.tabBtDefaultClass;
+                newView.tabBt.className = newView.tabBt.className.replace(defualtClass, activeClass);
+
+                this.currentViewIndex = newIndex;
+            }
+
+            this.__pos(-this.currentViewIndex * this.wrapperWidth);
         },
 
         __event: function (type) {
             var ev = document.createEvent("Event");
 
-            ev.initEvent('slideview-' + type, true, true);
+            ev.initEvent('tabgroup-' + type, true, true);
 
             this.wrapper.dispatchEvent(ev);
         },
