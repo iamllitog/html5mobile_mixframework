b53b3a193b1179715d6586156178f9e3330b039b
litong <litong.forme@gmail.com>
2014-10-31 17:55:27 +0800

    message

--- www/js/index.js
+++ www/js/index.js
@@ -1,52 +1,69 @@
 var page = {
-    cssObjs :{id : 'css',items : [{title:"栅格系统",id:'grid'},{title:"排版",id:'typesetting'},{title:"代码",id:'code'},{title:"表单",id:'form'},{title:"表格",id:'table'},{title:"按钮",id:'button'},{title:"图片",id:'image'},{title:"其他",id:'othercss'}]},
-    currentObjs : null,
-    sliderView : null,
-    tabGroup : null,
-    leftMenuList : null,
+    cssObjs: {id: 'css', items: [
+        {title: "栅格系统", id: 'grid'},
+        {title: "排版", id: 'typesetting'},
+        {title: "代码", id: 'code'},
+        {title: "表单", id: 'form'},
+        {title: "表格", id: 'table'},
+        {title: "按钮", id: 'button'},
+        {title: "图片", id: 'image'},
+        {title: "其他", id: 'othercss'}
+    ]},
+    componentsObjs: {id: 'components', items: [
+        {title: 'glyphions', id: 'glyphions'}
+    ]},
+    jspluginsObjs: {id: 'jsplugins', items: [
+        {title: 'glyphions', id: 'glyphions'}
+    ]},
+    currentObjs: null,
+    css: null,
+    components: null,
+    jsplugins: null,
+    sliderView: null,
+    tabGroup: null,
+    leftMenuList: null,
     initialize: function () {
         $(document).ready(this.documentReady);
 
     },
-    documentReady : function () {
+    documentReady: function () {
         page.initViews();
         page.bindEvents();
         page.initData();
-        
     },
-    initViews : function () {
+    initViews: function () {
         $('html').height($(window).height());//输入框盖住解决-1:输入框被盖住多因为高度按百分比，所以此处设置为死值
-        this.sliderView = new SlideView('#slidewrapper',{
-            openSlideMode : SlideView.OPEN_MODE_NONE
+        this.sliderView = new SlideView('#slidewrapper', {
+            openSlideMode: SlideView.OPEN_MODE_NONE
         });
         this.tabGroup = new TabGroup('#tabgroupwrapper');
         //需要加入click才能点击条目
-        this.leftMenuList = new IScroll('#leftmenuwrapper', { click: true ,tap: true});
+        this.leftMenuList = new IScroll('#leftmenuwrapper', { click: true, tap: true});
     },
     bindEvents: function () {
         //输入框盖住解决-2:当换向或resize时重新更改高度
-        $(window).on('onorientationchange' in window ? 'orientationchange' : 'resize',function(event){
+        $(window).on('onorientationchange' in window ? 'orientationchange' : 'resize', function (event) {
             $('html').height($(window).height());
         });
-        $('#slidewrapper').on('slideview-movein-leftview',function(event){
+        $('#slidewrapper').on('slideview-movein-leftview', function (event) {
             page.tabGroup.moveMode = TabGroup.MOVE_MODE_NODE;
         });
-        $('#slidewrapper').on('slideview-movein-centerview',function(event){
+        $('#slidewrapper').on('slideview-movein-centerview', function (event) {
             page.tabGroup.moveMode = TabGroup.MOVE_MODE_SLIDE;
         });
-        $('#menuButton').on('click',function(event){
+        $('#menuButton').on('click', function (event) {
             page.sliderView.toggleLeftView();
         });
 
-        $('#leftmenuSearchbar').on('input',function(event){
+        $('#leftmenuSearchbar').on('input', function (event) {
             var value = $(this).val();
-            if(value.lastIndexOf('\\') != -1){
+            if (value.lastIndexOf('\\') != -1) {
                 return;
             }
 
-            var searchList = {items : []};
+            var searchList = {items: []};
 
-            $.each(page.currentObjs.items, function(index, item){
+            $.each(page.currentObjs.items, function (index, item) {
                 if (page.currentObjs.items[index].title.match(value)) {
                     searchList.items.push(page.currentObjs.items[index]);
                 }
@@ -61,17 +78,29 @@ var page = {
         //当首次绑定元素时没有选择到指定元素(因为没有此元素),此时我们可以用代理事件绑定
         //如下 我们绑定到其父元素上 让其父元素代理事件
         //另外一个好处，就是可以减少事件的监听提高效率
-        $('#leftmenu_ul').on('tap','li',function(event){
-            if($(this).hasClass('active')) return;
+        $('#leftmenu_ul').on('tap', 'li', function (event) {
+            if ($(this).hasClass('active')) return;
             $('#leftmenu_ul > .active').removeClass('active');
             $(this).addClass('active');
+            page[page.tabGroup.getCurrentViewId()] = {title : $(this).text(),id:$(this).attr('data-id')};
             $('#title').text($(this).text());
-            var html = './' + $(this).attr('data-id')+'.html';
-            $("[data-tabgroup-tabviews] > [data-tabgroup-tabid='"+page.currentObjs.id+"'] > div").load(html);
+            var html = './' + $(this).attr('data-id') + '.html';
+            $("[data-tabgroup-tabviews] > [data-tabgroup-tabid='" + page.currentObjs.id + "'] > div").load(html);
             page.sliderView.toggleLeftView();
         });
+
+        $('#tabgroupwrapper').on('tabgroup-changeview', function (event) {
+//            alert(event.tabId);
+
+            page.currentObjs = page[event.tabId + 'Objs'];
+            var items = ich.left_menulist_template(page.currentObjs);
+            $('#leftmenu_ul li').remove();
+            $('#leftmenu_ul').append(items);
+
+            $("#leftmenu_ul >  [data-id='"+page[event.tabId].id+"'] ").addClass('active');
+        });
     },
-    initData : function(){
+    initData: function () {
         this.currentObjs = this.cssObjs;
         var cssItems = ich.left_menulist_template(this.currentObjs);
         $('#leftmenu_ul').append(cssItems);
@@ -82,6 +111,12 @@ var page = {
         $('#title').text($('#leftmenu_ul > li').first().text());
         page.leftMenuList.refresh();//当数据有变化时需要刷新
 
-        $("[data-tabgroup-tabviews] > [data-tabgroup-tabid='css']").load('./'+this.currentObjs.items[0].id+'.html');
+        this.css = this.cssObjs.items[0];
+        this.components = this.componentsObjs.items[0];
+        this.jsplugins = this.jspluginsObjs.items[0];
+
+        $("[data-tabgroup-tabviews] > [data-tabgroup-tabid='css']").load('./' + this.css.id + '.html');
+        $("[data-tabgroup-tabviews] > [data-tabgroup-tabid='components']").load('./' + this.components.id + '.html');
+        $("[data-tabgroup-tabviews] > [data-tabgroup-tabid='jsplugins']").load('./' + this.jsplugins.id + '.html');
     }
 };
